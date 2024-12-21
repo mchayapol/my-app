42cd4f720f7ead0f3ec40f105d4590c12bd4ae67
Chayapol Moemeng <mchayapol@gmail.com>
2024-12-21 11:08:52 +0700

    Add author name

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h1>Hello, World!</h1>
       <img src="/cat.png" alt="cat" width="400" height="400" />
+      <h2>by Chayapol</h2>
     </div>
   );
 }
